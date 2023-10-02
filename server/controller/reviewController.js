3139eb5172522e717edfcdb1258488162d7e30fb
Anshul45 <anshulgarwal025@gmail.com>
2023-10-02 22:53:35 +0530

    review routes created

--- server/controller/reviewController.js
+++ server/controller/reviewController.js
@@ -1,9 +1,41 @@
+import Review from "../models/reviewSchema.js";
+
 export const getAllReviews = async (req, res) => {
-  const review = await review.find({ productId: req.body.productId });
+  const product = req.body.productId;
+
+  if (product) {
+    const review = await Review.find({ productId: req.body.productId });
+    res.status(200).send(review);
+  } else {
+    res.status(404).send("No Review found");
+  }
 };
-export const postReview = (req, res) => {
-  res.send("It's working");
+
+export const postReview = async (req, res) => {
+  const user = req.user;
+  const addReview = new Review({
+    ...req.body,
+    userId: user._id,
+    username: user.name,
+  });
+  await addReview.save();
+  res.status(200).send("Review added successfully");
 };
-export const editReview = (req, res) => {
-  res.send("It's working");
+export const editReview = async (req, res) => {
+  const reviewId = req.headers.review_id;
+  const user = req.user._id;
+  const foundReview = await Review.findOne({ _id: reviewId });
+  if (foundReview.userId == user) {
+    await Review.findOneAndUpdate(
+      { _id: reviewId },
+      { ...req.body },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).send("Review updated successfully");
+  } else {
+    res.status(403).send("You are not authonticated");
+  }
 };
--- server/middleware/userMiddleware.js
+++ server/middleware/userMiddleware.js
@@ -1,13 +1,21 @@
 import jwt from "jsonwebtoken";
 import Auth from "../models/authSchema.js";
 
-export const verifyUser = async (req, res, next) => {
-  const token = req.body.accessToken;
+export const verifyUser = (req, res, next) => {
+  const token = req.headers.accesstoken;
 
-  if (!token) res.status(403).send("You Are Not authorized");
-
-  const user = jwt.verify(token, process.env.JWT_SECRET);
-
-  req.user = await Auth.findOne({ email: user.email });
-  next();
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
+      if (err) {
+        res.status(403).send("You Are Not authorized");
+      }
+      const data = await Auth.findOne({ email: user.email }).select(
+        "-password"
+      );
+      req.user = data;
+      next();
+    });
+  } else {
+    res.status(403).send("You Are Not authorized");
+  }
 };
