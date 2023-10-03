659c94a1ebc4900db0c729d49c56562960a16a9b
Anshul45 <anshulgarwal025@gmail.com>
2023-10-03 22:27:20 +0530

    created header

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,28 +1,38 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
-
 const Header = () => {
   return (
-    <div className="flex flex-col gap-2 bg-red-400">
-      <div className="flex justify-between py-3 px-1">
-        <div>
-          <h1>Hemburger</h1>
+    <div className="flex flex-col gap-2  shadow-md">
+      <div className="flex justify-between items-center py-3 px-5">
+        <div className="flex gap-5 items-center">
+          <MenuIcon className="hover:bg-gray-100 cursor-pointer" />
+          <div>
+            <SearchIcon className="hover:bg-gray-100 cursor-pointer" />
+            <input
+              className="w-56 p-1 outline-none hover:bg-gray-50"
+              placeholder=" What are you looking for?"
+            />
+          </div>
         </div>
-        <div>
-          <img src="../../assets/logo.png" />
+        <div className="pr-60">
+          <img src="../../assets/logo.png" className="cursor-pointer" />
         </div>
-        <div className="flex gap-2 ">
-          <h1>user</h1>
-          <h1>cart</h1>
+        <div className="flex gap-5">
+          <AccountCircleIcon className="hover:bg-gray-100 cursor-pointer" />
+          <ShoppingCartIcon className="hover:bg-gray-100  cursor-pointer" />
         </div>
       </div>
-      <div className="flex justify-center">
-        <hr className="w-[82%] shadow-2xl" />
+      <div className="flex justify-center ">
+        <hr className="w-[82%]" />
       </div>
-      <div className="flex justify-center gap-5 pb-3">
-        <h1>Home</h1>
-        <h1>Shop All</h1>
-        <h1>Category1</h1>
-        <h1>Category2</h1>
+      <div className="flex justify-center  gap-5 pt-2  pb-5">
+        <h1 className=" cursor-pointer">Home</h1>
+        <h1 className="  cursor-pointer">Shop All</h1>
+        <h1 className="  cursor-pointer">Category1</h1>
+        <h1 className="  cursor-pointer">Category2</h1>
       </div>
     </div>
   );
