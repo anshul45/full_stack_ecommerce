df4dc54f8191fcb0b5bd7e5cf827e776415d06dd
Anshul45 <anshulgarwal025@gmail.com>
2023-10-17 23:54:55 +0530

    commit

--- client/src/page/Error.js
+++ client/src/page/Error.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "../assets/error.png";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 mt-16 ">
+      <img src={Image} alt="error image" className="w-72 h-72" />
+      <div>Page not found</div>
+      <div>Sorry, this page does not exist.</div>
+      <button>BACK TO HOME</button>
+    </div>
+  );
 };
 
 export default Error;
